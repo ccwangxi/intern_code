af2cee337810688800a373f9e3e74fad3f1deebc
ccwangxi <502273497@qq.com>
2018-03-16 10:23:36 -0400

    Update Trp53.R

--- Trp53.R
+++ Trp53.R
@@ -1,22 +1,22 @@
 #########################
-## Author: Xi Wang
+## Author: XXXXXXXXX
 ## Date: 2018/02/21
-## Title: Find Trp53 
+## Title: Find AAAAA gene 
 ##     mutation in vep table
 #########################
 library(dplyr)
 library(biomaRt)#get gene name
 
-#trp53 location in NCBI: chr11 69580359..69591873
+#tAAAA location in NCBI: chrn, start,end
 
-anndir <- "/data1/workspace/DCI/Kirsch/Chang-Lung.Lee/WES_2018/Results/VEP/vep_output/"
-outdir <- "/data1/workspace/DCI/Kirsch/Chang-Lung.Lee/WES_2018/Analysis/Xi_VEP/"
+anndir <- ###########
+outdir <- ###########
 
 
-files <- list.files(path=anndir, pattern = '.tsv') #VEP table genereted by Dadong
-file2 <- '/home.local/jsg57/kirsch_project/group3_table.tsv' #VEP table denerated by Jeremy
+files <- list.files(path=anndir, pattern = '.tsv') #VEP table genereted by person 1
+file2 <- ########## #VEP table denerated by person 2
 
-get_trp53mut <-function(file,chrnum=11,trp_start = 69580359,trp_end = 69591873 ){
+get_AAAmut <-function(file,chrnum=chrn,AA_start = start,AA_end = end ){
     vepfiltfile <- paste0(anndir, file)
     tools::md5sum(vepfiltfile)
     vep <- read.table(vepfiltfile, sep=' ', header=FALSE, stringsAsFactors=FALSE)
@@ -24,33 +24,33 @@ get_trp53mut <-function(file,chrnum=11,trp_start = 69580359,trp_end = 69591873 )
                         "EnsGene", "EnsFeature", 
                         "Annotation", "Impact")
     vep['sample']=substr(file,1,3)
-    trp_mut <- vep %>% filter(chr==chrnum, (start>=trp_start & start<=trp_end) | (end>=trp_start & end<=trp_end))
-    return(trp_mut)
+    AA_mut <- vep %>% filter(chr==chrnum, (start>=AA_start & start<=AA_end) | (end>=AA_start & end<=AA_end))
+    return(AA_mut)
 }
 
-trp_mut_df={}
+AA_mut_df={}
 for (i in 1:length(files)){
-    trp_mut_df = rbind(trp_mut_df,get_trp53mut(files[i]))
+    AA_mut_df = rbind(AA_mut_df,get_AAAmut(files[i]))
 }
-dim(trp_mut_df)
+dim(AA_mut_df)
 #0 9
 
 #reproduce the result using file2
-get_trp53mut2 <-function(file,chrnum=11,trp_start = 69580359,trp_end = 69591873 ){
+get_AAAmut2 <-function(file,chrnum=chrn,AA_start = start,AA_end = end){
     vepfiltfile <- file
     tools::md5sum(vepfiltfile)
     vep <- read.table(vepfiltfile, sep=' ', header=FALSE, stringsAsFactors=FALSE)
     colnames(vep) <- c("sample","chr", "start", "end", "ALT", 
                         "EnsGene", "EnsFeature", 
                         "Annotation", "Impact")
-    trp_mut <- vep %>% filter(chr==chrnum, (start>=trp_start & start<=trp_end) | (end>=trp_start & end<=trp_end))
-    #trp_mut <- vep %>% filter(sample %in% c('S28','S29','S31','S32','S33'))
-    return(trp_mut)
+    AA_mut <- vep %>% filter(chr==chrnum, (start>=AA_start & start<=AA_end) | (end>=AA_start & end<=AA_end))
+    #AA_mut <- vep %>% filter(sample %in% XXXXXXXXXX)
+    return(AA_mut)
 }
-trp_mut_df2 <- get_trp53mut2(file2)
-dim(trp_mut_df2)
-#write.csv(trp_mut_df2,file=paste0(outdir,'trp53_mut.csv'))
-sum(trp_mut_df2[,-1]!=trp_mut_df[,-9])
+AA_mut_df2 <- get_AAAmut2(file2)
+dim(AA_mut_df2)
+#write.csv(AA_mut_df2,file=paste0(outdir,'AAA_mut.csv'))
+sum(AA_mut_df2[,-1]!=AA_mut_df[,-9])
 #0: the two vep generate the same results.
 
 
@@ -92,20 +92,10 @@ for (i in 1:length(files)){
 count
 
 
-
-
-
-#  sample Ensgene_n gene_n agg_gene_n
-#1    S28     10183  10167       9626
-#2    S29      1979   1977       1426
-#3    S31      1973   1972       1413
-#4    S32      2299   2296       1619
-#5    S33      2007   2002       1417
 # Ensgene_n is the number of unique EnsGene ID 
 # gene_n is the number of unique gene names after assigning the gene name from BioMart database to each EnsGene ID.
-# agg_gene_n is the number of unique gene names after aggregating the same mutation to one gene name. (because same genetic position might be assigned to different gene names)
-
-#saved in gene_count.txt under the same folder.
+# agg_gene_n is the number of unique gene names after aggregating the same mutation to one gene name. 
+                         #(because same genetic position might be assigned to different gene names)
 
 
 
